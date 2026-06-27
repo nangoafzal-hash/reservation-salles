234d23dd4757c300013d6b0242f304c4158569cf
Manou Afzal <nangoafzal@gmail.com>
2026-06-28 01:31:02 +0200

    commit 4 : ajout liste creneaux dans Salle

--- src/Salle.java
+++ src/Salle.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
 public class Salle {
     private String nom;
     private int capacite;
+    private ArrayList<String> creneaux;
     
     public Salle(String nom, int capacite) {
         this.nom = nom;
         this.capacite = capacite;
+        this.creneaux = new ArrayList<>();
     }
     public String getNom() {
         return nom;
@@ -13,4 +16,6 @@ public class Salle {
     public int getCapacite() {
         return capacite;
     }
+    public ArrayList<String> getCreneaux() {
+         return creneaux; }
 }
