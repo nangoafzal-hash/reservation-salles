8ffb6ef165b8dbb902cc8064c056d7f7159f05ac
CyberGhost3010 <josephkoalaga30@gmail.com>
2026-06-28 11:11:39 +0000

    commit 7 : ajouterReservation

--- src/GestionnaireReservation.java
+++ src/GestionnaireReservation.java
@@ -16,4 +16,15 @@ public class GestionnaireReservation {
         }
         return false; 
     }
+
+    
+     public boolean ajouterReservation(Salle salle, String creneau, String nomUtilisateur) {
+        if (estDisponible(salle, creneau)) {
+            System.out.println("refusé : " + salle.getNom() + " déjà prise à " + creneau);
+            return false;
+        }
+        listeReservations.add(new Reservation(nomUtilisateur, salle, creneau));
+        System.out.println("ok : " + nomUtilisateur + " réserve " + salle.getNom() + " à " + creneau);
+        return true;
+    }
 }
--- src/Reservation.java
+++ src/Reservation.java
@@ -1,10 +1,14 @@
 
 public class Reservation {
+
+    static void add(Reservation reservation) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     private String nomUtilisateur;
     private Salle salle;
     private String creneau;
 
-    public Reservation(String nomUtilisateur, Salle salle, String creneau) {
+    public Reservation(Salle salle, String nomUtilisateur, String creneau) {
         this.nomUtilisateur = nomUtilisateur;
         this.salle = salle;
         this.creneau = creneau;
@@ -21,4 +25,9 @@ public class Reservation {
     public String getCreneau() {
         return creneau;
     }
+
+    public static void add(Reservation reservation) {
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
 }
