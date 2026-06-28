679ee83a2b62fabac8d59e99bbedbaede4fddece
Kevin KIBA <kibakevin46@gmail.com>
2026-06-28 15:45:54 +0000

    commit 8 : ajout de annulerReservation

--- src/GestionnaireReservation.java
+++ src/GestionnaireReservation.java
@@ -11,20 +11,23 @@ public class GestionnaireReservation {
         for (Reservation r : listeReservations) {
             if (r.getSalle().getNom().equals(salle.getNom())
                 && r.getCreneau().equals(creneau)) {
-                return true; 
+                return true;  
             }
         }
         return false; 
     }
 
-    
-     public boolean ajouterReservation(Salle salle, String creneau, String nomUtilisateur) {
-        if (estDisponible(salle, creneau)) {
+    public boolean ajouterReservation(Salle salle, String creneau, String nomUtilisateur) {
+        if (!estDisponible(salle, creneau)) {
             System.out.println("refusé : " + salle.getNom() + " déjà prise à " + creneau);
             return false;
         }
         listeReservations.add(new Reservation(nomUtilisateur, salle, creneau));
         System.out.println("ok : " + nomUtilisateur + " réserve " + salle.getNom() + " à " + creneau);
         return true;
+    }
+
+    public void annulerReservation(Reservation res) {
+        listeReservations.remove(res);
     }
 }
